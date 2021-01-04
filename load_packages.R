6144e43b498f73211dcbc592221599f5d928ba31
Alex Baecher <56590356+slamander@users.noreply.github.com>
2021-01-04 14:38:49 -0500

    Update load_packages to include package updates
    Added update.packages() condition to automatically install all newer versions of packages

--- load_packages.R
+++ load_packages.R
@@ -1,9 +1,13 @@
 load_packages <- function(x){
-  installed_packages <- x %in% rownames(installed.packages())
-  if (any(installed_packages == FALSE)) {
+  installed_packages <- packages %in% rownames(installed.packages())
+  if (any(installed_packages == F)){
     install.packages(packages[!installed_packages])
   }
   
-  invisible(
-    lapply(x, library, character.only = TRUE))
+  outdated_packages <- packages %in% rownames(old.packages())
+  if (any(outdated_packages == T)){
+    install.packages(packages[outdated_packages])
+    }
+  
+  invisible(lapply(packages, library, character.only = TRUE))
 }
